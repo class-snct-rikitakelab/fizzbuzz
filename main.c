abb20182a061c821470a4c6111de45aebc553aa3
AbeYusaku <abeyusaku@Saturn.(none)>
2015-01-20 15:39:36 +0900

    fizzbuzz

--- main.c
+++ main.c
@@ -3,12 +3,17 @@
 int main(){
 	int input;
 	scanf("%d",&input);
-	printf("%d\n",input);
+
 
 	if((input % 3)==0){
+		if (input%5==0){
+			printf("fizzbuzz\n");
+		}
+		else{
 		printf("fizz\n");
+		}
 	}
-	if((input % 5)==0){
+	else if((input % 5)==0){
 		printf("buzz\n");
 	}
 	else{
